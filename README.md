2a782db1764d86187e2feaf8d274314345385dfd
Allan Tatter <allan@optimistdigital.com>
2020-04-08 15:00:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Laravel Generate Storage Structure
+
+This package generates the Laravel storage folder structure. Useful when mounting an empty directory to replace `storage/` in production or staging environments.
+
+When running a Laravel application with an empty storage directory, you should get an error related with reading/storing files to/from `storage` directory like `ErrorException
+file_put_contents(/../storage/..): failed to open stream: No such file or directory` or similar. This can be fixed by manually creating the folder structure as needed, but it's cumbersome task with automated CI/CD flows.
+
+## Usage
+
+```
+composer require optimistdigital/laravel-generate-storage-structure
+```
+
+Create `app/`, `framework/` and `logs/` directories into the current directory:
+
+```
+generate-storage-structure
+```
+
+Create `app/`, `framework/` and `logs/` directories into `/storage` directory:
+
+```
+generate-storage-structure --storage-path=/storage
+```
---
+++ src/Commands/GenerateStorageStructureCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace OptimistDigital\GenerateStorageStructure\Commands;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateStorageStructureCommand extends Command
+{
+    protected static $defaultName = 'generate-storage-structure';
+
+    protected function configure()
+    {
+        $this
+            ->setDefinition(
+                new InputDefinition([
+                    new InputOption('storage-path', null, InputOption::VALUE_REQUIRED),
+                ])
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filesystem = new Filesystem;
+
+        $rootDirectory = $input->getOption('storage-path');
+        if ($rootDirectory !== null) {
+            $rootDirectory = rtrim($rootDirectory, '/');
+        }
+
+        $directories = [
+            'app',
+            'app/public',
+            'framework',
+            'framework/cache',
+            'framework/sessions',
+            'framework/testing',
+            'framework/views',
+            'logs',
+        ];
+
+        foreach ($directories as $directory) {
+            if ($rootDirectory !== null) {
+                $directory = $rootDirectory.'/'.$directory;
+            }
+
+            if ($filesystem->exists($directory)) {
+                continue;
+            }
+
+            $filesystem->mkdir($directory);
+        }
+
+        $output->writeln('Storage structure generated.');
+
+        return 0;
+    }
+}
